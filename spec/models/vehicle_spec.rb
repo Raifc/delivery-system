1158966ba488faee12846c573821cc5aac453e25
Raif <raif.si@hotmail.com>
2022-05-16 05:05:38 -0300

    vehicle unit test done

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    context 'presence'
+    it 'should create a new vehicle' do
+      address = Address.new(full_address: '100, 1st street', city: 'New York', state: 'New York')
+      email_domain = EmailDomain.new(domain: 'alfa@alfa.com')
+      company = Company.new(corporate_name: 'Beta', trading_name: 'Alfa', registration_number: '1234567', address: address, email_domain: email_domain)
+      vehicle = Vehicle.new(license_plate: 'ABF123', brand: 'Volkswagen', model: 'Van', year: '1992', load_capacity:'200', company: company)
+
+      expect(vehicle).to be_valid
+
+
+    end
+  end
 end
